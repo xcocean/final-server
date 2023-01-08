76ba23a7fbd7cfdd49fb3b8df9cd900fbf342e53
lingkang <910405656@qq.com>
2023-01-08 17:02:24 +0800

    添加自定义模板渲染文档

---
+++ example/src/main/java/top/lingkang/finalserver/example/app/MyHttpParseTemplate.java
@@ -0,0 +1,25 @@
+package top.lingkang.finalserver.example.app;
+
+import org.springframework.stereotype.Component;
+import top.lingkang.finalserver.server.core.HttpParseTemplate;
+
+import java.util.Map;
+
+/**
+ * @author lingkang
+ * Created by 2023/1/8
+ * 自定义模板渲染
+ */
+@Component
+public class MyHttpParseTemplate implements HttpParseTemplate {
+    @Override
+    public void init(String templatePath) {
+
+    }
+
+    @Override
+    public String getTemplate(String template, Map map) throws Exception {
+        // 在此处进行模板渲染，返回输出结果
+        return null;
+    }
+}
