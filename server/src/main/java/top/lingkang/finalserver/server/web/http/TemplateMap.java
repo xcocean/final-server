6402dceb1610fc437e8e4ff60ba0c3dae1343710
lingkang <910405656@qq.com>
2023-01-16 22:24:45 +0800

    优化map

--- server/src/main/java/top/lingkang/finalserver/server/web/http/TemplateMap.java
+++ server/src/main/java/top/lingkang/finalserver/server/web/http/TemplateMap.java
@@ -7,12 +7,15 @@ import java.util.Map;
  * @author lingkang
  * 2023/1/16
  * @since 1.0.0
- * put 已经被重新，无法返回旧value
  **/
 public class TemplateMap extends HashMap<String, Object> implements Map<String, Object> {
-    @Override
-    public TemplateMap put(String key, Object value) {
-        super.put(key, value);
+    public TemplateMap add(String key, Object value) {
+        put(key, value);
+        return this;
+    }
+
+    public TemplateMap addAll(Map<String, Object> map) {
+        putAll(map);
         return this;
     }
 }
